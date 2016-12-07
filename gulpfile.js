b262f977b040d518b9e85bdd18fd4bdeaee9dea9
Eduardo Alvarez <eduardo.a1993@gmail.com>
2016-12-07 13:14:28 -0300

    Incoporar tarea que compila sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 const gulp = require("gulp")
 const browserSync = require("browser-sync").create()
 const reload = browserSync.reload
+const sass = require('gulp-sass')
+
 
 // Task server
 gulp.task("server", () => {
@@ -13,11 +15,21 @@ gulp.task("server", () => {
 		}
 	})
 })
+
 // Task watch
 gulp.task("watch", () => {
 	gulp.watch("./index.html").on("change", reload)
-	gulp.watch("./public/gb_styles.sass").on('change', reload)
-	gulp.watch("./public/gb_script.ts").on('change', reload)
+	gulp.watch("./assets/css/gb_styles.css").on('change', reload)
+	gulp.watch("./assets/js/gb_script.js").on('change', reload)
+	gulp.watch('./public/*.scss', ['sass'])
+})
+
+// Task to compilate Sass
+gulp.task('sass', () => {
+  	return gulp.src('./public/*.scss')
+    	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    	.pipe(gulp.dest('./assets/css/'))
+    	.pipe(browserSync.stream())
 })
 
 // Task default
